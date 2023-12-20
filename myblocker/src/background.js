7a0ca7135a5f355f779904aa90842cb855097128
sklab <takamineu@gmail.com>
2023-12-20 12:50:48 +0900

    Refactoring

--- myblocker/src/background.js
+++ myblocker/src/background.js
@@ -1,49 +1,39 @@
-// ストレージからルール情報を取得し更新する関数
+// Resource types for filtering conditions
+const ALL_RESOURCE_TYPE = ['beacon', 'csp_report', 'font', 'image', 'main_frame', 'media', 'object', 'other', 'ping', 'script', 'sub_frame', 'websocket', 'xmlhttprequest'];
+
+// Update rules from storage
 const updateRules = async () => {
   let { urls } = await chrome.storage.local.get('urls');
   urls = urls || [];
 
-  // 現在のルールをすべて取得
   const currentRules = await chrome.declarativeNetRequest.getDynamicRules();
   const ruleIds = currentRules.map(rule => rule.id);
-  
-  // 現在のルールをすべて削除
+  // Remove all rules
   await chrome.declarativeNetRequest.updateDynamicRules({ removeRuleIds: ruleIds });
 
-  let rules = [];
-  let idCount = 0;
-  for (const url of urls) {
-    // 空のURLを無視
-    if (url.trim() === '') continue;
-    // コメント行を無視
-    if (url.trim().startsWith('//')) continue;
-
-    idCount++;
-
-    // 新しいURLブロックルールを作成する
-    const rule = {
-      id: idCount,
-      priority: idCount,
+  const rules = urls
+    .filter(url => url.trim() !== '' && !url.trim().startsWith('#'))
+    .map((url, index) => ({
+      id: index + 1,
+      priority: index + 1,
       action: { type: 'block' },
-      condition: { urlFilter: url, resourceTypes: ['main_frame', 'image', 'media', 'object', 'other', 'script',
-      'sub_frame', 'webbundle', 'websocket', 'ping', 'csp_report', 'xmlhttprequest'] }
-    };
-    rules.push(rule);
-  }
-
+      condition: {
+        urlFilter: url,
+        resourceTypes: ALL_RESOURCE_TYPE
+      }
+    }));
   if (rules.length > 0) {
-    // ルールを追加する
     await chrome.declarativeNetRequest.updateDynamicRules({ addRules: rules });
   }
 
 };
 
-// ストレージが更新されたときにルールを更新する
+// Update rules when storage changes
 chrome.storage.onChanged.addListener(() => {
   updateRules();
 });
 
-// 拡張機能がインストールまたはアップデートされた時にルールを更新
+// Update rules when extension is installed
 chrome.runtime.onInstalled.addListener(() => {
   updateRules();
 });
